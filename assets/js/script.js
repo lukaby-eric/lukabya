a72f1fdfd9a7df62498f8295d51dd053e389804c
Lukas lubingo <78145529+lukaby-eric@users.noreply.github.com>
2024-04-08 20:33:21 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,37 +32,37 @@ function closeJobs() {
 function vicklines() {
   projectImage.src="../assets/images/vic.png";
   projectTitle.innerHTML="Vicklines Barbershop";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I'm currently working on this website for a client in Kansas City who runs a cozy barbershop. I've created versions using both Wordpress and a custom design with HTML, CSS, Bootstrap, and JavaScript.";
   projectLink.href="https://lukaby-eric.github.io/vick/";
 }
 function rankfuse() {
   projectImage.src="../assets/images/rankk.png";
   projectTitle.innerHTML="Rank Fuse";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I came across a digital marketing website with a design that I absolutely loved, so I decided to challenge myself and recreate it using my front-end development skills. I built its landing page from the ground up using HTML, CSS, Bootstrap, and JavaScript.";
   projectLink.href="https://lukaby-eric.github.io/rank-fuse/";
 }
 function wineclub() {
   projectImage.src="../assets/images/win.png";
   projectTitle.innerHTML="Louie's Wine Club";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I collaborated with Martina Elena web design company to develop a website for a client who owns a large wine shop in Overland Park, KS and Des Moines, IA. The site was designed using HTML, CSS, JavaScript, and Bootstrap.";
   projectLink.href="https://lukaby-eric.github.io/wineClub/";
 }
 function mcm() {
   projectImage.src="../assets/images/mcmm.png";
   projectTitle.innerHTML="Martin City Marketing";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I came across a digital marketing website with a design that I absolutely loved, so I decided to challenge myself and recreate it using my front-end development skills. I built its landing page from the ground up using HTML, CSS, Bootstrap, and JavaScript.";
   projectLink.href="https://lukaby-eric.github.io/mcm/";
 }
 function unity() {
   projectImage.src="../assets/images/unit.png";
   projectTitle.innerHTML="Unity Village";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I came across a non profit business website with a design that I absolutely loved, so I decided to challenge myself and recreate it using my front-end development skills. I built its landing page from the ground up using HTML, CSS, Bootstrap, and JavaScript.";
   projectLink.href="https://lukaby-eric.github.io/unity/";
 }
 function flash() {
   projectImage.src="../assets/images/flashh.png";
   projectTitle.innerHTML="The Flash";
-  projectDescription.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat saepe nemo culpa enim quae, ratione delectus doloremque voluptas nisi aut cum laborum obcaecati magnam veritatis nam ea dolore. Accusantium";
+  projectDescription.innerHTML="I created this website as a fun project, drawing inspiration from the sleek and user-friendly design of Netflix's webpage. Intrigued by the idea, I chose to reimagine a landing page for one of my favorite shows at the time, ``The Flash`` Utilizing my skills in HTML, CSS, and JavaScript, I meticulously crafted each element to mirror the aesthetic and functionality of the streaming platform. From the layout to the interactive features, every detail was carefully considered to provide an engaging and immersive user experience. This project allowed me to not only showcase my technical abilities but also express my passion for design and creativity.";
   projectLink.href="https://lukaby-eric.github.io/netflix/";
 }
 
